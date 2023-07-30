fc149b97d3b66de272a827f203b664de140b9a0d
NalinBista <112367459+NalinBista@users.noreply.github.com>
2023-07-30 21:30:00 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,138 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>About</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<?php
+		$Username = null;
+		if(!empty($_SESSION["Username"]))
+		{
+			$Username = $_SESSION["Username"];
+		}
+	?>
+</head>
+
+<body>
+
+    <div class="brand">SneakerFusion</div>
+    <div class="address-bar"><strong>Premium</strong> Shoes for Everyone</div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">SneakerFusion</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                   <li><a href="index.php">Home</a></li>
+					<li><a href="bestseller.php">Best Sellers</a></li>
+					<li><a href="shop.php">Shop</a></li>
+                    <li><a href="about.php">About</a></li>
+					<li><a href="#" onclick="ManagementOnclick();">Management</a></li>
+					<?php if($Username == null){echo '<li><a href="register.php?ActionType=Register">Register</a></li>';} ?>
+					<?php if($Username == null){echo '<li><a href="Login.php?Role=User">Login</a></li>';} else {echo '<li><a href="Logout.php">Logout</a></li>';} ?>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>SneakerFusion</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                    <img class="img-responsive img-border-left" src="img/slide3.jpg" alt="">
+                </div>
+                <div class="col-md-6">
+                    <p>SneakerFusion, an innovative online sneaker store, has swiftly emerged as a trendsetting hub for sneaker enthusiasts worldwide. Founded with a passion for kicks, SneakerFusion stands as a digital mecca for all sneakerheads seeking to elevate their sneaker game. Established in 2023, the store has since echoed its name in the hearts of sneaker aficionados through its vast collection of rare, exclusive, and limited-edition sneakers. The team at SneakerFusion continuously strives to bring new sneaker trends, exciting releases, and unbeatable customer service, cementing its place as a global leader in the sneaker industry.</p>
+                
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                   <p>
+					<?php echo '<strong>'.$Username.'</strong>'; ?>
+					<br>
+					<strong>
+					<?php if($Username != null){echo '<a href="ManageAccount.php?Role=User">Manage Account</a> |';} ?> 
+					<?php if($Username == null){echo '<a href="Login.php?Role=User">Login</a>';} else {echo '<a href="Logout.php">Logout</a>';} ?> | 
+					<a href="#">Back to top</a>
+					</strong><br>
+					Copyright &copy; SneakerFusion Store 2023
+					</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	<script>
+		
+		function ManagementOnclick(){
+			if(confirm("Only Administrators are allowed in this page, Please Login as an Administrator.") == true)
+			{
+				window.open("Login.php?Role=Admin","_self",null,true);
+			}
+		}
+		
+    </script>
+
+</body>
+
+</html>
